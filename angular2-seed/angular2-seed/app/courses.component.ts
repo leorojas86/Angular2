706f841207245cf48cd6f5dd6400f75f4b5fe0c4
Leonardo Rojas <lrojas-as@avantica.net>
2016-07-29 13:55:39 -0600

    implementing node js service

---
+++ angular2-seed/angular2-seed/app/courses.component.ts
@@ -0,0 +1,14 @@
+import {Component} from 'angular2/core'
+
+@Component
+(
+    {
+        selector : 'courses',
+        template : '<h2>Courses<h2>'
+    }
+)
+
+export class CoursesComponent 
+{
+
+}
